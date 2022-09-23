0bfd26e6e7093747eb8103812312ee8581e800e7
sarthak713 <sarthakbhardwaj713@gmail.com>
2022-09-23 18:45:25 +0530

    new file

---
+++ 21arraysLeetCodeQues.cpp
@@ -0,0 +1,90 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+
+// Rotate Array
+    // vector<int> arr={1,2,3,4,5};
+    // int n=5;
+    // int k=3;
+    // vector<int> temp(n);
+    // for(int i=0;i<n;i++){   
+    //     temp[(i+k)%n]=arr[i];
+    // }
+    // for(int i=0;i<n;i++){
+    //     cout<<temp[i]<<" ";
+    // }cout<<endl;
+
+// Check if array is Sorted & Rotated
+    // vector<int> arr={4,5,1,2,3};
+    // int n=5;
+    // int count=0;
+    // for(int i=1;i<n;i++){
+    //     if(arr[i-1]>arr[i]){
+    //         count++;
+    //     }
+    // }
+    // if(arr[n-1]>arr[0]){
+    //     count++;
+    // }
+    // if(count>1){
+    //     cout<<false<<endl;
+    // }else{
+    //     cout<<true<<endl;
+    // }
+ 
+// Sum of 2 arrays
+    // int arr1[]={1,2,3,4};
+    // int arr2[]={6};
+    // int n=4;
+    // int m=1;
+    // int arr1[]={1,2,3};
+    // int arr2[]={9,9};
+    // int n=3;
+    // int m=2;
+    /*
+    carry = sum/10
+    sum = sum%10
+    arr[i] = sum
+    */
+    // int i=n-1;
+    // int j=m-1;
+    // int carry=0;
+    // vector<int>ans;
+    // while(i>=0 && j>=0){
+    //    int val1=arr1[i]; 
+    //    int val2=arr2[j]; 
+    //    int sum=val1+val2+carry;
+    //    carry=sum/10;
+    //    sum=sum%10;
+    //    ans.push_back(sum);
+    //    i--;j--;
+    // }
+    // while(i>=0){
+    //     int sum=arr1[i]+carry;
+    //     carry=sum/10;
+    //     sum=sum%10;
+    //     ans.push_back(sum);
+    //     i--;
+    // }
+    // while(j>=0){
+    //     int sum=arr2[j]+carry;
+    //     carry=sum/10;
+    //     sum=sum%10;
+    //     ans.push_back(sum);
+    //     j--;
+    // }
+    // while(carry!=0){
+    //     int sum=carry;
+    //     carry=sum/10;
+    //     sum=sum%10;
+    //     ans.push_back(sum);
+    // }
+    // reverse(ans);
+    
+
+    
+    
+    return 0;
+}
