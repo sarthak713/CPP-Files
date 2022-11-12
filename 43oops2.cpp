5e6cc38b9d7cd1148273f565291f36e5c05a80c0
sarthak <sarthakbhardwaj713@gmail.com>
2022-11-12 19:10:58 +0530

    OOPs concepts 1

---
+++ 43oops2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include"42oops1.cpp"
+using namespace std;
+
+int main(){
+    
+// NewFile class is defined in 42oops1.cpp file 
+// But can be used here by using the above #include statement
+
+    NewFile n1;
+    
+    return 0;
+}
